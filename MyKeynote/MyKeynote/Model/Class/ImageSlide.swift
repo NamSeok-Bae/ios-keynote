873afa04a9e6013ddb3c86775f89092d38023925
NamSeok-Bae <qoskatjr2@gmail.com>
2023-07-27 01:29:11 +0900

    feat: ImageSlide에 size와 imageString 변수 추가 및 관련 함수 구현

--- MyKeynote/MyKeynote/Model/Class/ImageSlide.swift
+++ MyKeynote/MyKeynote/Model/Class/ImageSlide.swift
@@ -11,4 +11,18 @@ class ImageSlide: Slide, Equatable {
     static func == (lhs: ImageSlide, rhs: ImageSlide) -> Bool {
         return lhs.description == rhs.description
     }
+    
+    private(set) var size: CGSize = CGSize(width: 100, height: 100)
+    private(set) var imageString: String = ""
+    
+    func updateImageString(data: Data?) {
+        if let data {
+            let base64 = data.base64EncodedString()
+            imageString = base64
+        }
+    }
+    
+    func updateSize(size: CGSize) {
+        self.size = size
+    }
 }
