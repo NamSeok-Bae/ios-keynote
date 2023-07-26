34e273e865e1265222c59eefdf6e236413d9180f
NamSeok-Bae <qoskatjr2@gmail.com>
2023-07-27 01:30:49 +0900

    chore: setBackgroundColor 함수를 2개의 함수로 분리

--- MyKeynote/MyKeynote/Util/Extension/CALayer+.swift
+++ MyKeynote/MyKeynote/Util/Extension/CALayer+.swift
@@ -12,11 +12,11 @@ extension CALayer {
         return self.backgroundColor?.alpha ?? 1
     }
     
-    func setBackgroundColorWithAlpha(color: SlideColor, alpha: SlideAlpha? = nil) {
-        if let alpha {
-            backgroundColor = UIColor(color: color).withAlphaComponent(alpha.cgValue).cgColor
-        } else {
-            backgroundColor = UIColor(color: color).withAlphaComponent(backgroundColorAlpha).cgColor
-        }
+    func setBackgroundColor(color: SlideColor) {
+        backgroundColor = UIColor(color: color).withAlphaComponent(backgroundColorAlpha).cgColor
+    }
+    
+    func setBackgroundColorWithAlpha(color: SlideColor, alpha: SlideAlpha) {
+        backgroundColor = UIColor(color: color).withAlphaComponent(alpha.cgValue).cgColor
     }
 }
