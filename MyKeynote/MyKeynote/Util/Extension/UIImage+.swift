8a50abd941e70fc4ba781957c6d9c5a791c26a7f
NamSeok-Bae <qoskatjr2@gmail.com>
2023-07-27 10:26:16 +0900

    add: 누락된 UIImage 파일 추가

---
+++ MyKeynote/MyKeynote/Util/Extension/UIImage+.swift
@@ -0,0 +1,34 @@
+//
+//  UIImage+.swift
+//  MyKeynote
+//
+//  Created by 배남석 on 2023/07/26.
+//
+
+import UIKit
+
+extension UIImage {
+    static let rectangle = UIImage(systemName: "rectangle.center.inset.filled")
+    static let image = UIImage(systemName: "photo")
+    
+    convenience init(slide: Slide) {
+        self.init(systemName: slide is SquareSlide ? "rectangle.center.inset.filled" : "photo")!
+    }
+    
+    func resize(standardSize: CGSize) -> UIImage {
+        let scaleByWidth = standardSize.width / self.size.width
+        let scaleByHeight = standardSize.height / self.size.height
+        
+        let scale = scaleByWidth > scaleByHeight ? scaleByHeight : scaleByWidth
+        let newWidth = self.size.width * scale
+        let newHeight = self.size.height * scale
+
+        let size = CGSize(width: newWidth, height: newHeight)
+        let render = UIGraphicsImageRenderer(size: size)
+        let renderImage = render.image { context in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
+        
+        return renderImage
+    }
+}
